d6163c06d81098ce9b3007a2a7a2e64d2ee5694b
Noah Heckenlively <heckenna@rose-hulman.edu>
2022-11-25 20:34:06 -0600

    Logistic regression works
    Needs comments and maybe a few bugs went unnoticed.

--- LogiReg/R/LogiReg.R
+++ LogiReg/R/LogiReg.R
@@ -12,36 +12,64 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-logistic_regression = function(x_train, y_train) {
-  beta = logistic_regression_trainer(x_train, y_train)
+x_train = read.csv2("D:/DDox/ML/Project/ML_FinalProject_Team17/cardio_train.csv", sep = ";")
+y_train = x_train["cardio"]
+x_train <- subset(x_train, select = -c(1, 2, 3, 4,5, 6, 13))
+x_train = head(x_train, 100)
+y_train = head(y_train, 100)
+
+x_train = as.matrix(x_train)
+
+
+#logistic_regression(x_train,y_train, 20)
+#(beta_next = logistic_regression_trainer_helper(beta_next, data, targs, lr = .0001))
+#y_pred = logistic_reg_predict_dataset(x_train, beta_next)
+#loss(y_train, y_pred)
+
+loss = function(y_pred, y_train) {
+  sum((y_pred - y_train)^2)
+}
+
+logistic_regression = function(x_train, y_train, num_epochs = 20) {
+  beta_cur = logistic_regression_trainer(x_train, y_train, num_epochs = num_epochs)
   
-  return(logistic_reg_predict_dataset(x_train, beta))
+  return(logistic_reg_predict_dataset(x_train, beta_cur))
 }
 
 logistic_reg_predict_dataset = function(x_train, beta) {
   y_pred = rep(0,length(x_train[,1]))
+  data = as.matrix(x_train)
+  
+  temp_func = function(row) {predict_row(beta, row)}
   
   for (i in 1:length(x_train[,1])) {
     row = x_train[i,]
     y_pred[i] = predict_row(beta, row)
   }
   
+  # Wanted to vectorize it... Rstudio just crashed instead.
+  #y_pred = apply(X = data, MARGIN = 1, FUN = temp_func)
+  
   return(y_pred)
 }
 
 logistic_regression_trainer <- function(x_train, y_train, num_epochs = 20) {
   # Takes in dataframe and returns beta for best fit. 
-  beta_init = solve(t(x_train) %*% x_train) %*% x_train %*% y_train
+  data = as.matrix(x_train) # Cast to matrix so things work
+  targs = as.matrix(y_train)
+  
+  beta_init = solve(t(data) %*% data) %*% t(data) %*% targs
+  
   beta_cur = beta_init
   
   
   
   for (i in 1:num_epochs){
-    beta_next = logistic_regression_trainer_helper(beta_cur, x_train, y_train)
+    beta_next = logistic_regression_trainer_helper(beta_cur, data, targs)
     
-    if (is_close(beta_next, beta_cur)) {
-      break
-    }
+    #if (is_close(beta_next, beta_cur)) {
+    #  break
+    #}
   }
   
   return(beta_cur)
@@ -56,23 +84,23 @@ is_close = function(b1, b2){
   return(FALSE)
 }
 
-logistic_regression_trainer_helper = function(beta, x_train, y_train) {
-  beta_cur = beta
+logistic_regression_trainer_helper = function(beta_init, x_train, y_train, lr = 1) {
+  beta_cur  = beta_init
   weight_changes = rep(0, length(beta_cur))
-  lr = 1
-  
+
   for (i in 1:length(x_train[,1])){
-    row = x_train[,i]
+    row = x_train[i,]
     y_i = y_train[i]
     
     s_i = predict_row(beta_cur, row)
     
-    row_grad = (y_i -  s_i)*row
-    Weight_changes = weight_changes - row_grad
+    row_grad = (y_i -  s_i) * row
+    
+    weight_changes = weight_changes - row_grad
   }
   
   # Update beta_cur vector
-  beta_cur = beta_cur + weight_changes*lr*(1/length(x_train[,1]))
+  beta_cur = beta_cur - weight_changes*lr*(1/length(x_train[,1]))
   
   return(beta_cur)
 }
@@ -82,6 +110,6 @@ predict_row = function(beta, row) {
 }
 
 sigmoid = function(beta, x_vec) {
-  1/(1 + exp(-t(x_vec)%*%beta))
+  1/(1 + exp(-as.vector(x_vec) %*% as.vector(beta)))
 }
 
