bcf3794552db0eee9864de71964f37f27ba3a879
Prokash Deb <prokashdeb@gmail.com>
2022-11-27 14:37:30 -0600

    Idea of Confusion Matrix

--- LogiReg.Rcheck/00_pkg_src/LogiReg/R/LogiReg.R
+++ LogiReg.Rcheck/00_pkg_src/LogiReg/R/LogiReg.R
@@ -12,21 +12,13 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-# x_train = read.csv2("D:/DDox/ML/Project/ML_FinalProject_Team17/cardio_train.csv", sep = ";")
-# y_train = x_train["cardio"]
-# x_train <- subset(x_train, select = -c(1, 2, 3, 4,5, 6, 13))
-# x_train = head(x_train, 100)
-# y_train = head(y_train, 100)
-#
-# x_train = as.matrix(x_train)
+x_train = read.csv2("D:/DDox/ML/Project/ML_FinalProject_Team17/cardio_train.csv", sep = ";")
+y_train = x_train["cardio"]
+x_train <- subset(x_train, select = -c(1, 2, 3, 4,5, 6, 13))
+x_train = head(x_train, 100)
+y_train = head(y_train, 100)
 
-
-# uploading mtcars data from tidyverse library
-
-# df <- mtcars
-# x_train <- df[, c(8,9)]
-# y_train <- df[, 1]
-# x_train = as.matrix(x_train)
+x_train = as.matrix(x_train)
 
 
 #logistic_regression(x_train,y_train, 20)
@@ -40,46 +32,46 @@ loss = function(y_pred, y_train) {
 
 logistic_regression = function(x_train, y_train, num_epochs = 20) {
   beta_cur = logistic_regression_trainer(x_train, y_train, num_epochs = num_epochs)
-
+  
   return(logistic_reg_predict_dataset(x_train, beta_cur))
 }
 
 logistic_reg_predict_dataset = function(x_train, beta) {
   y_pred = rep(0,length(x_train[,1]))
   data = as.matrix(x_train)
-
+  
   temp_func = function(row) {predict_row(beta, row)}
-
+  
   for (i in 1:length(x_train[,1])) {
     row = x_train[i,]
     y_pred[i] = predict_row(beta, row)
   }
-
+  
   # Wanted to vectorize it... Rstudio just crashed instead.
   #y_pred = apply(X = data, MARGIN = 1, FUN = temp_func)
-
+  
   return(y_pred)
 }
 
 logistic_regression_trainer <- function(x_train, y_train, num_epochs = 20) {
-  # Takes in dataframe and returns beta for best fit.
+  # Takes in dataframe and returns beta for best fit. 
   data = as.matrix(x_train) # Cast to matrix so things work
   targs = as.matrix(y_train)
-
+  
   beta_init = solve(t(data) %*% data) %*% t(data) %*% targs
-
+  
   beta_cur = beta_init
-
-
-
+  
+  
+  
   for (i in 1:num_epochs){
     beta_next = logistic_regression_trainer_helper(beta_cur, data, targs)
-
+    
     #if (is_close(beta_next, beta_cur)) {
     #  break
     #}
   }
-
+  
   return(beta_cur)
 }
 
@@ -99,17 +91,17 @@ logistic_regression_trainer_helper = function(beta_init, x_train, y_train, lr = 
   for (i in 1:length(x_train[,1])){
     row = x_train[i,]
     y_i = y_train[i]
-
+    
     s_i = predict_row(beta_cur, row)
-
+    
     row_grad = (y_i -  s_i) * row
-
+    
     weight_changes = weight_changes - row_grad
   }
-
+  
   # Update beta_cur vector
   beta_cur = beta_cur - weight_changes*lr*(1/length(x_train[,1]))
-
+  
   return(beta_cur)
 }
 
@@ -120,57 +112,4 @@ predict_row = function(beta, row) {
 sigmoid = function(beta, x_vec) {
   1/(1 + exp(-as.vector(x_vec) %*% as.vector(beta)))
 }
-
-
-#### This is the IDEA of how we can find the Confusion Matrix.
-
-confusion_matrix <- function(y_pred, y_train){
-  y_pred = ifelse(y_pred>0.5, 1, 0)
-
-  matrix_table = table(y_pred, y_train)
-
-
-  prevalence = matrix_table[4]/sum(matrix_table[1:4])
-  accuracy = sum(matrix_table[1], matrix_table[4])/sum(matrix_table[1:4])
-  sensitivity = matrix_table[4] / sum(matrix_table[4], matrix_table[3])
-  specificity = matrix_table[1] / matrix_table(cm[1], matrix_table[2])
-  fscore = (2 * (sensitivity * Prevalence))/(sensitivity + Prevalence)
-  DOR = (matrix_table[4]/matrix_table[3])/(matrix_table[2]/matrix_table[1])
-
-  return(matrix_table, prevalence, accuracy, sensitivity, specificity, fscore, DOR)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
