490468a016f9f794401e583e0380ad41b741597c
Prokash Deb <prokashdeb@gmail.com>
2022-12-02 13:22:09 -0600

    some minor Plot function modification

--- R/LogiReg.R
+++ R/LogiReg.R
@@ -275,10 +275,10 @@ logireg_Plot<-function(x_train,y_train,beta,color="steelblue",line_width=2){
   new_x_train<-cbind(x_train[,1],new_x_train)
   new_x_train[,1]<-x_train[,1]
   p_hat<-logistic_reg_predict_dataset(x_train,beta)
-  p_hat<-ifelse(p_hat>0.5, 1, 0)
-  datause<-cbind(p_hat,x_train[,1])
+  p_hat1<-ifelse(p_hat>0.5, 1, 0)
+  datause<-cbind(p_hat1,x_train[,1],p_hat)
   plot(datause[,2],y_train, col=color,xlab="x",ylab="predict")
-  lines(datause[,2],datause[,1], lwd=line_width)
+  lines(sort(datause[,2], decreasing = T), sort(datause[,3], decreasing = T), lwd=line_width)
 }
 
 
